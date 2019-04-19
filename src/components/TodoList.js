5a42980b4776d258799b5e64dd8a8c2523cfea77
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-04-19 08:41:27 +0300

    Add props for TodoListItem: label, important

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,8 +4,11 @@ import TodoListItem from './TodoListItem';
 const TodoList = () => {
   return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li>
+      <li><TodoListItem label="Drink Coffee" /></li>
+      <li><TodoListItem 
+        label="Build React App"
+        important
+      /></li>
     </ul>
   );
 };
