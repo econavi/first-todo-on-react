9b21a03e04e22d207a0b15deb668eebc689288cc
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-04-19 08:59:53 +0300

    Add array "todoData" for property TodoList

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import AppHeader from './components/AppHeader';
 import SearchPanel from './components/SearchPanel';
 
 const App = () => {
+  
+  const todoData = [
+    { label: 'Drink Coffee', important: false, },
+    { label: 'Make Awesome App', important: true, },
+    { label: 'Have a lunch', important: false, },
+  ];
+  
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData} />
     </div>
   );
 };
