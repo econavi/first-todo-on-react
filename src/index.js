23e4e8256df1f41803254a15c75aadb6c64e6b08
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-04-19 10:16:41 +0300

    Rename files to components, add styles to App, add more components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './components/TodoList';
-import AppHeader from './components/AppHeader';
-import SearchPanel from './components/SearchPanel';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
 
 const App = () => {
-  
+
   const todoData = [
-    { id: '1', label: 'Drink Coffee', important: false, },
-    { id: '2', label: 'Make Awesome App', important: true, },
-    { id: '3', label: 'Have a lunch', important: false, },
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
   ];
-  
+
   return (
-    <div>
-      <AppHeader />
-      <SearchPanel />
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
       <TodoList todos={todoData} />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
